6bc2a4f02cdefd6704bc5d9df0cc868e5ac665f7
halfspin-qc <70982212+halfspin-qc@users.noreply.github.com>
2021-04-10 17:02:06 -0700

    Update gates.py

--- gates.py
+++ gates.py
@@ -22,28 +22,58 @@ from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister, execute
 from qiskit.visualization import plot_histogram
 
 
+
+# This is the basic circuit to start with
 n = 2
-qc_output = QuantumCircuit(n,n)
+base_circ = QuantumCircuit(n,n)
 for j in range(n):
-    qc_output.h(j)
-    qc_output.measure(j,j)
+    base_circ.h(j)
+    base_circ.measure(j,j)
 
-qc_output.draw()
+base_circ.draw()
 
 
-###
+### Add the Gate letter to the circuit
+def H_Gate():
+    circ_new.h(0)
 
-n = 2
-qc_output = QuantumCircuit(n,n)
-for j in range(n):
-    qc_output.h(j)
-    qc_output.measure(j,j)
+def X_Gate():
+    circ_new.x(0)
 
-qc_output.draw()
+def Y_Gate():
+    circ_new.y(0)
+
+def Z_Gate():
+    circ_new.z(0)
+
+def default():
+    return "Invalid Gate!"
+
+switch_case = {
+  1: H_Gate,
+  2: X_Gate,
+  3: Y_Gate,
+  4: Z_Gate
+}
+
+def switch(x):
+    return switch_case.get(x, default)()
+
+circ_new = QuantumCircuit(2,2)
+switch(1)   ## hardocding here, but the argument needs to be filled from the button the user chooses to add
+circ_new.draw()
 
 ####
+measure_all = QuantumCircuit(2,2)
+measure_all.measure([0,1],[0,1])
+measure_all.draw()
+final_qc = base_circ+circ_new+measure_all
+#final_qc.draw()
+
+
+
 shots = 1000
-counts = execute(qc_output,Aer.get_backend('qasm_simulator'),shots=shots).result().get_counts()
+counts = execute(final_qc,Aer.get_backend('qasm_simulator'),shots=shots).result().get_counts()
 
 #### 
 probs00 = counts.get('00') /shots
